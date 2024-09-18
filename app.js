931e667b93df0979492bdd27c72aa650c3892e6e
NhanB2105586 <nhanb2105586@student.ctu.edu.vn>
2024-09-18 23:23:40 +0700

    Định nghĩa các route cho Tài nguyên Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 // Cho phép các yêu cầu từ mọi nguồn
 app.use(cors());
@@ -14,6 +15,7 @@ app.get('/', (req, res) => {
   // Trả về một response với dữ liệu JSON
   res.json({ message: 'Welcome to contact book application.' });
 });
+app.use("/api/contacts", contactsRouter);
 
 // Export ứng dụng để sử dụng ở các file khác
 module.exports = app;
